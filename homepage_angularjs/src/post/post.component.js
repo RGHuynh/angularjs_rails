bbbfde667b7efa5d09806bea85b9135815b22caf
RGHuynh <randall.g.huynh@gmail.com>
2018-06-19 13:37:25 -0400

    fix: post to dynamically update body in database

--- homepage_angularjs/src/post/post.component.js
+++ homepage_angularjs/src/post/post.component.js
@@ -18,14 +18,7 @@
         url: "http://localhost:3000/posts/" + id + '.json',
         data: {body: newPost}
       }).then(function successCallback(response){
-        console.log(response);
-        for(var i = 0; i < ctrl.posts.length; i++){
-          if(ctrl.posts[i].id === id){
-            ctrl.posts.splice(i, 1);
-            ctrl.posts.push(response.data);
-            break;
-          }
-        }
+        ctrl.post.body = response.data.body;
       });
     };
 
